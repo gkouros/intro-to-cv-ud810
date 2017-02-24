Fix image gradient calculation

ps5_python/lk_optic_flow.py
@@ -12,9 +12,9 @@ def lk_optic_flow(frame1, frame2, win=2):
     Ix = np.zeros(frame1.shape, dtype=np.float32)
     Iy = np.zeros(frame1.shape, dtype=np.float32)
     It = np.zeros(frame1.shape, dtype=np.float32)
-    Ix[1:-1, 1:-1] = (frame1[1:-1, 2:] - frame1[1:-1, :-2]) / 2
-    Iy[1:-1, 1:-1] = (frame1[2:, 1:-1] - frame1[:-2, 1:-1]) / 2
-    It[1:-1, 1:-1] = frame1[1:-1, 1:-1] - frame2[1:-1, 1:-1]
+    Ix[1:-1, 1:-1] = cv2.subtract(frame1[1:-1, 2:], frame1[1:-1, :-2]) / 2
+    Iy[1:-1, 1:-1] = cv2.subtract(frame1[2:, 1:-1], frame1[:-2, 1:-1]) / 2
+    It[1:-1, 1:-1] = cv2.subtract(frame1[1:-1, 1:-1], frame2[1:-1, 1:-1])
 
     params = np.zeros(frame1.shape + (5,))
     params[..., 0] = Ix ** 2
@@ -50,38 +50,27 @@ def my_lk_optic_flow(frame1, frame2, filter_size=5, threshold=0):
     '''
     My lucas kanade optic flow implementation
     '''
-    It = cv2.GaussianBlur(frame2 - frame1, (5,5), 0)
-    Ix = cv2.GaussianBlur(cv2.Sobel(frame1, cv2.CV_64F, 1, 0, 5), (5,5), 0)
-    Iy = cv2.GaussianBlur(cv2.Sobel(frame1, cv2.CV_64F, 0, 1, 5), (5,5), 0)
-    Ixx = Ix * Ix
-    Iyy = Iy * Iy
-    Ixy = Ix * Iy
-    Ixt = Ix * It
-    Iyt = Iy * It
+
+    It = cv2.subtract(frame1, frame2).astype(np.float32)
+    Ix, Iy = np.gradient(frame1)
+    Ix = Ix.astype(np.float32)
+    Ix = Ix.astype(np.float32)
 
     # calculate the gradient product accumulations for each pixel
-    Sxx = cv2.boxFilter(Ixx, -1, ksize=(filter_size,)*2, normalize=True)
-    Sxy = cv2.boxFilter(Ixy, -1, ksize=(filter_size,)*2, normalize=True)
-    Syy = cv2.boxFilter(Iyy, -1, ksize=(filter_size,)*2, normalize=True)
-    Sxt = cv2.boxFilter(Ixt, -1, ksize=(filter_size,)*2, normalize=True)
-    Syt = cv2.boxFilter(Iyt, -1, ksize=(filter_size,)*2, normalize=True)
-    del It, Ix, Iy, Ixx, Ixy, Iyy, Ixt, Iyt
+    Sxx = cv2.boxFilter(Ix**2, -1, ksize=(filter_size,)*2, normalize=True)
+    Sxy = cv2.boxFilter(Ix*Iy, -1, ksize=(filter_size,)*2, normalize=True)
+    Syy = cv2.boxFilter(Iy**2, -1, ksize=(filter_size,)*2, normalize=True)
+    Sxt = cv2.boxFilter(Ix*It, -1, ksize=(filter_size,)*2, normalize=True)
+    Syt = cv2.boxFilter(Iy*It, -1, ksize=(filter_size,)*2, normalize=True)
+    del It, Ix, Iy
+
     # calculate the displacement matrices U, V
     rows, cols = frame1.shape
-    flow = np.zeros((rows, cols, 2), dtype=np.float64)
+    flow = np.zeros((rows, cols, 2), dtype=np.float32)
     A = np.dstack((Sxx, Sxy, Sxy, Syy))
     b = np.dstack((-Sxt, -Syt))
-    #  threshold = 100
     for r in range(rows):
         for c in range(cols):
             flow[r,c,:] = np.linalg.lstsq(A[r,c].reshape((2,2)), b[r,c])[0]
-            #  ev,_ = np.linalg.eig(A[r,c].reshape((2,2)))
-            #  if ev[1] >= threshold and ev[0] >= threshold:
-                #  flow[r,c,:] = np.dot(np.linalg.inv(A[r,c].reshape((2,2))), b[r,c])
-            #  elif ev[1] >= threshold and ev[0] < threshold:
-                #  flow[r,c,:] = np.linalg.lstsq(A[r,c].reshape((2,2)), b[r,c])[0]
-            #  else:
-                #  flow[r,c,:] = 0
 
-    flow = -flow.astype(np.float32)
-    return flow
+    return -flow

ps5_python/ps5.py
@@ -25,14 +25,14 @@ def ps5_1_a():
     #  fr1 = cv2.imread('input/DataSeq2/0.png', cv2.IMREAD_GRAYSCALE)
     #  fr2 = cv2.imread('input/DataSeq2/1.png', cv2.IMREAD_GRAYSCALE)
     #  fr3 = cv2.imread('input/DataSeq2/2.png', cv2.IMREAD_GRAYSCALE)
-    #  flow12 = cv2.calcOpticalFlowFarneback(fr2, fr1, None,
+    #  flow12 = cv2.calcOpticalFlowFarneback(fr1, fr2, None,
                                           #  0.5, 3, 15, 3, 5, 1.2, 0)
-    flow12 = lk_optic_flow(fr2, fr1, 15)
+    flow12 = lk_optic_flow(fr1, fr2, 15)
     flow13 = lk_optic_flow(fr1, fr3, 15)
     vis_optic_flow_arrows(fr1, flow12, 'output/ps5-1-a-1.png', show=False)
     vis_optic_flow_arrows(fr1, flow13, 'output/ps5-1-a-2.png', show=False)
-    #  cv2.imwrite('output/lala/flow1.jpg', vis_optic_flow(flow12))
-    #  cv2.imwrite('output/lala/flow2.jpg', vis_optic_flow(flow13))
+    cv2.imwrite('output/lala/flow1.jpg', vis_optic_flow(flow12))
+    cv2.imwrite('output/lala/flow2.jpg', vis_optic_flow(flow13))
 
 
 
@@ -73,7 +73,6 @@ def ps5_3_a_1():
     plot_n_save('output/ps5-3-a-2.png', diffs)
     cv2.imwrite('input/DataSeq1/yos_img_1_warped.jpg', warps[0])
     cv2.imwrite('input/DataSeq1/yos_img_2_warped.jpg', warps[1])
-    vis = []
     for i, flow in enumerate(flows):
         cv2.imwrite('output/lala/flow1'+str(i)+'.jpg', vis_optic_flow(flow))
 
@@ -82,10 +81,9 @@ def ps5_3_a_2():
     path = lambda i: 'input/DataSeq2/' + str(i) + '.png'
     frs = np.array([cv2.imread(path(i), cv2.IMREAD_GRAYSCALE) for i in range(3)])
     # apply LK to the sequence of images using constant level
-    flows, warps, diffs = single_level_lk(frs, levels=2, window=25)
+    flows, warps, diffs = single_level_lk(frs, levels=3, window=7)
     vis_optic_flow_arrows_multi(frs, flows, 'output/ps5-3-a-3.png')
     plot_n_save('output/ps5-3-a-4.png', diffs)
-    vis = []
     for i, flow in enumerate(flows):
         cv2.imwrite('output/lala/flow2'+str(i)+'.png', vis_optic_flow(flow))
 

ps5_python/vis_optic_flow.py
@@ -40,7 +40,7 @@ def vis_optic_flow_arrows_multi(images, flows, filename, show=False):
         plot_idx = rows * 100 + cols * 10 + 1 + idx
         plt.subplot(plot_idx)
         plt.imshow(img, cmap='gray', interpolation='bicubic')
-        step=img.shape[0] / 15
+        step=img.shape[0] / 30
         x = np.arange(0, img.shape[1], 1)
         y = np.arange(0, img.shape[0], 1)
         x, y = np.meshgrid(x, y)